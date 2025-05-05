7befcecf4a2f2c2b49b3e4e272f6f79a5f5761fb
ash <ashkernel02@gmail.com>
2025-05-05 11:56:13 -0600

    most request specific things seem to be in the context parameter, which makes sense. Should research more.

--- main.go
+++ main.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"net/http"
+	//"net/url"
 	"github.com/gin-gonic/gin"
 	"flag"
 	"fmt"
 )
 
-func respond(c *gin.Context) {
+func respondPing(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
 }
 
+func catchallFunc(c *gin.Context) {
+	fmt.Println("c.FullPath(): ", c.FullPath())
+	fmt.Println("c.request.URL: ", c.Request.URL)
+	fmt.Println("c.request.URL.path: ", c.Request.URL.Path)
+}
+
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/ping", respond)
+
+	//r.GET("/ping", respondPing)
+
+	// catchall route
+	r.Any("/*proxyPath", catchallFunc)
 	return r
 }
 
