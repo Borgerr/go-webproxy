9fc728e04e4e9f2bdfeffefa87b7e1f466f238ba
ash <ashkernel02@gmail.com>
2025-05-07 13:59:17 -0600

    weird issue where request is redirected just by default

--- main.go
+++ main.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"net/http"
-	//"net/url"
 	"github.com/gin-gonic/gin"
 	"flag"
 	"fmt"
+	"io"
 )
 
-func respondPing(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
-}
-
-func catchallFunc(c *gin.Context) {
+func guy(c *gin.Context) {
 	fmt.Println("c.FullPath(): ", c.FullPath())
 	fmt.Println("c.request.URL: ", c.Request.URL)
 	fmt.Println("c.request.URL.path: ", c.Request.URL.Path)
+
+	res, err := http.Get(c.Request.URL.String())
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		// TODO: how do we handle this?
+	}
+
+	byteString, err := io.ReadAll(res.Body)
+	resString := string(byteString)
+	fmt.Println("resString: %s", resString)
+
+	fmt.Println("status for client: %s", res.Status)
+	//c.String(res.String())
 }
 
 func setupRouter() *gin.Engine {
@@ -26,7 +33,7 @@ func setupRouter() *gin.Engine {
 	//r.GET("/ping", respondPing)
 
 	// catchall route
-	r.Any("/*proxyPath", catchallFunc)
+	r.Any("/*proxyPath", guy)
 	return r
 }
 
